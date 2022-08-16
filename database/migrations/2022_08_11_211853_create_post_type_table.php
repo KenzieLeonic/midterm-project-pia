35a41a205428246cc889d161b9108b627f2c9344
aungpor <aungpor.napat@gmail.com>
2022-08-16 22:05:27 +0700

    test

--- database/migrations/2022_08_11_211853_create_post_type_table.php
+++ database/migrations/2022_08_11_211853_create_post_type_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignIdFor(\App\Models\Post::class);   // `post_id`
             $table->foreignIdFor(\App\Models\Type::class);   // `type_id`
             $table->timestamps();
+            //test
         });
     }
 
